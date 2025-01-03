81e19b7b9d724f4810692234936cf717fc234185
Rex Wang <cping.wang@gmail.com>
2025-01-03 10:19:22 +0800

    Rename ConfigBean to CarConfigurationBeans

--- src/main/java/com/example/ioc/configuration/CarConfigurationBeans.java
+++ src/main/java/com/example/ioc/configuration/CarConfigurationBeans.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ConfigBeans {
+public class CarConfigurationBeans {
 
     @Bean
     public Car car(){
